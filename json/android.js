2ba5c50285298b06db3585f43aecedb3644fa4bb
yamanemre367 <yamanemre367@gmail.com>
2025-05-26 11:11:59 +0300

    Update android.js

--- json/android.js
+++ json/android.js
@@ -1,30 +1,28 @@
 [
     {
-      " Traktor ismi": "Tümosan",
-      "Traktor Model": "2018",
-      "Traktor Özellik": "motor Gücü:115hp Vites:5ileri,1Geri Konfor olarak tümosan kendini baya geliştirmiştir ve kendini geliştirmeye devam etmektedir.",
-      "Model Yapımcısı": "TÜrk MOtor SANayi ve Ticaret Anonim Şirketi'yapmıştır.",
-      "gorsel": "https://github.com/yamanemre367/android/blob/main/t%C3%BCmosan.jpg",
+       "isim": "Tümosan",
+    "kalori": "2018",
+    "karbonhidrat": "motor Gücü:115hp Vites:5ileri,1Geri Konfor olarak tümosan kendini baya geliştirmiştir ve kendini geliştirmeye devam etmektedir.",
+    "protein": "TÜrk MOtor SANayi ve Ticaret Anonim Şirketi'yapmıştır.",
+    "gorsel": "https://github.com/yamanemre367/android/blob/main/t%C3%BCmosan.jpg"
+    {
+       "isim": "fent",
+    "kalori": "2021",
+    "karbonhidrat": "motor Gücü:380hp,Vites:6ileri.1Geri,6silindir olup dünya Çapında tercih edilen Traktörlerden bir tanesidir.",
+    "protein": "AGCO Corporation",
+    "gorsel": "https://github.com/yamanemre367/android/blob/main/fent.png"
     },
     {
-       " Traktor ismi": "fent",
-        "Traktor Model": "2021",
-        "Traktor Özellik": "motor Gücü:380hp,Vites:6ileri.1Geri,6silindir olup dünya Çapında tercih edilen Traktörlerden bir tanesidir.",
-        "Model Yapımcısı": "AGCO Corporation",
-        "gorsel": "https://github.com/yamanemre367/android/blob/main/fent.png"
+       "isim": "Massey Ferguson",
+    "kalori": "2024",
+    "karbonhidrat": "motor Gücü:265 HP,Vites Sistemi: 7 ileri,3Geri, olup Kendisin dünya çapında ismini tanıtmıştir ve daha iybir hale getirmiştir",
+    "protein": "Massey-Harris ve İngiliz Ferguson Limited ile beraber kurmuşlardır.",
+    "gorsel": "https://github.com/yamanemre367/android/blob/main/masey%20ferguson-4.png"
     },
     {
-        " Traktor ismi": "Massey Ferguson",
-        "Traktor Model": "2024",
-        "Traktor Özellik": "motor Gücü:265 HP,Vites Sistemi: 7 ileri,3Geri, olup Kendisin dünya çapında ismini tanıtmıştir ve daha iybir hale getirmiştir",
-        "Model Yapımcısı": "Massey-Harris ve İngiliz Ferguson Limited ile beraber kurmuşlardır.",
-        "gorsel": "https://github.com/yamanemre367/android/blob/main/masey%20ferguson-4.png"
-    },
-    {
-        " Traktor ismi": "  New Holland",
-        "Traktor Model": "2022",
-        "Traktor Özellik": "Motor Gücü:110hp,Vites:12 ileri,12 geri,kullanılmıştır.Bu traktor kendini gücü olarak tanıtmıştır ve daha iyi bir hale getirmiştir ve getirmeye devam etmektedir.",
-        "Model Yapımcısı": "",
-        "gorsel": "https://github.com/yamanemre367/android/blob/main/new%20holland-tasarim-(1).jpg"
-    }
+        "isim": " New Holland",
+    "kalori": "2022",
+    "karbonhidrat": "Motor Gücü:110hp,Vites:12 ileri,12 geri,kullanılmıştır.Bu traktor kendini gücü olarak tanıtmıştır ve daha iyi bir hale getirmiştir ve getirmeye devam etmektedir.",
+    "protein": "CNH Industrial",
+    "gorsel": "https://github.com/yamanemre367/android/blob/main/new%20holland-tasarim-(1).jpg"
   ]
